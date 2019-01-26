e8a09519da4d97574d2104487848feea151dcb89
Sajor <565785929@qq.com>
2019-01-26 23:43:22 +0800

    use flask-session

--- d07/testCookie/manage.py
+++ d07/testCookie/manage.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_script import Manager
+from flask_session import Session
 from App.views import blue
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Study Flask'
+app.config['SESSION_TYPE'] = 'redis'
+Session(app)
+
 app.register_blueprint(blueprint=blue)
 manager = Manager(app=app)
 
